0ec310cad3823d0128c5b148998c27a0dbe0b09b
Vladyslav <vlad.moiseev1399@gmail.com>
2023-07-27 18:44:31 -0400

    - fixing js errors; - search template;

--- src/Controller/Custom/CustomHomepageController.php
+++ src/Controller/Custom/CustomHomepageController.php
@@ -7,7 +7,6 @@ namespace BitBag\OpenMarketplace\Controller\Custom;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
 final class CustomHomepageController extends AbstractController
@@ -16,10 +15,8 @@ final class CustomHomepageController extends AbstractController
     {
     }
 
-    #[Route('/', name: 'sylius_shop_homepage')]
     public function indexAction(Request $request): Response
     {
-        dd(222);
         return new Response($this->templatingEngine->render('@SyliusShop/Homepage/index.html.twig'));
     }
 }
